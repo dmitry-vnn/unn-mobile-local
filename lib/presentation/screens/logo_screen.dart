010de06532521331cd5929c5fd510edf71725812
Dmitry <xtopz1337@gmail.com>
2023-10-27 23:19:40 +0300

    change: beautify logo screen

--- lib/presentation/screens/logo_screen.dart
+++ lib/presentation/screens/logo_screen.dart
@@ -5,14 +5,36 @@ class LogoScreen extends StatelessWidget {
 
   	@override
   	Widget build(BuildContext context) {
-		return const Scaffold(
+		return Scaffold(
 			body: Center(
-				child: Image(
-					image: AssetImage("assets/images/logo.png")
+				child: Column(
+					crossAxisAlignment: CrossAxisAlignment.center,
+					mainAxisAlignment: MainAxisAlignment.center,
+
+					children: [
+						_logoTitle("УНИВЕРСИТЕТ"),
+						_logoTitle("ЛОБАЧЕВСКОГО"),
+						const SizedBox(height: 30),
+						const Image(
+				    		image: AssetImage("assets/images/logo.png")
+						),
+				  ],
 				)
 			)
 		);
   	}
+
+	Text _logoTitle(String title) {
+		return Text(
+			title,
+			style: const TextStyle(
+				color: Color.fromRGBO(15, 104, 170, 1.0),
+				fontSize: 34.09,
+				fontFamily: "LetoSans"
+			),
+			textAlign: TextAlign.center,
+		);
+	}
 
 
 
