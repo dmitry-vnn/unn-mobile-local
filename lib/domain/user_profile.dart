90ff4b5021e49fe592810550269c5f5ffba8b5a8
Dmitry <xtopz1337@gmail.com>
2023-10-27 13:16:20 +0300

    change: add eduGroup property. make avatar nullable

--- lib/domain/user_profile.dart
+++ lib/domain/user_profile.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
 
 class UserProfile {
+
 	final String firstName;
 	final String lastName;
-	final Image avatar;
+	final String educationGroup;
+	final Image? avatar;
 
-	UserProfile(this.firstName, this.lastName, this.avatar);
+	UserProfile(this.firstName, this.lastName, this.educationGroup, {this.avatar});
 }
